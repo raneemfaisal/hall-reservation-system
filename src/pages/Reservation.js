a3ef517b437bd57f99c5f5440e18e216c13a1d5c
raneemfaisal <77919227+raneemfaisal@users.noreply.github.com>
2022-08-25 14:36:09 +0300

    Update Reservation.js

--- src/pages/Reservation.js
+++ src/pages/Reservation.js
@@ -1,4 +1,4 @@
-import React , {useState} from 'react';
+import React , {useState, useEffect} from 'react';
 import '../styles/reservationStyle.css';
 import { Link } from "react-router-dom";
 import { IconContext } from "react-icons";
@@ -9,8 +9,16 @@ import DropdownType from './DropdownType';
 import DropdownHalls from './DropdownHalls';
 import Time from './Time';
 import { colRefReserve } from '../firebase-config';
-import { addDoc } from 'firebase/firestore'
-import ReactJsAlert from "reactjs-alert"
+import { addDoc } from 'firebase/firestore';
+import ReactJsAlert from "reactjs-alert";
+// import { colRefHall } from '../firebase-config';
+// import { getDocs } from 'firebase/firestore';
+import {
+  ref,
+  getDownloadURL,
+  listAll,
+} from "firebase/storage";
+import { storage } from '../firebase-config';
 
 
 function Reservation() {
@@ -24,6 +32,123 @@ function Reservation() {
     const [type, setType] = useState("");
     const [title, setTitle] = useState("");
 
+    const [imageUrls, setImageUrls] = useState([]);
+
+  
+    const images = (imagesListRef) =>{
+      listAll(imagesListRef).then((response) => {
+        response.items.forEach((item) => {
+          getDownloadURL(item).then((url) => {
+            setImageUrls((prev) => [...prev, url]);
+          });
+        });
+      });
+    }
+  useEffect(() => {
+    
+      if(selected === 'المكتبة المركزية للطالبات'){
+        if(selected2 === 'فردية'){
+        const imagesListRef = ref(storage, "TheCentralLibrary/Single/");
+        images(imagesListRef);
+      }
+      if(selected2 === 'مشتركة'){
+        const imagesListRef = ref(storage, "TheCentralLibrary/Other/");
+        images(imagesListRef);
+      }
+      }
+
+      if(selected === "مكتبة كلية الطب"){
+        const imagesListRef = ref(storage, "SchoolOfMedicine");
+        images(imagesListRef);
+      }
+
+       if(selected === "مكتبة كلية التمريض"){
+        if(selected2 === 'فردية'){
+          const imagesListRef = ref(storage, "NursingCollegeLibrary/Single/");
+          images(imagesListRef);
+        }
+        if(selected2 === 'مشتركة'){
+          const imagesListRef = ref(storage, "NursingCollegeLibrary/Other/");
+          images(imagesListRef);
+        }
+      }
+
+      if(selected === "مكتبة كلية الدراسات التطبيقية وخدمة المجتمع"){
+        if(selected2 === 'فردية'){
+          const imagesListRef = ref(storage, "LibraryOfTheCollegeAppliedStudies/Single/");
+          images(imagesListRef);
+        }
+        if(selected2 === 'مشتركة'){
+          const imagesListRef = ref(storage, "LibraryOfTheCollegeAppliedStudies/Other/");
+          images(imagesListRef);
+        }
+      }
+
+      if(selected === "مكتبة كلية العلوم الطبية التطبيقية"){
+        if(selected2 === 'فردية'){
+          const imagesListRef = ref(storage, "LibraryOfTheCollegeOfAppliedMedicalSciences/Single/");
+          images(imagesListRef);
+        }
+        if(selected2 === 'مشتركة'){
+          const imagesListRef = ref(storage, "LibraryOfTheCollegeOfAppliedMedicalSciences/Other/");
+          images(imagesListRef);
+        }
+      }
+
+      if(selected === "مكتبة كلية طب الأسنان"){
+        if(selected2 === 'فردية'){
+          const imagesListRef = ref(storage, "LibraryOfTheCollegeOfDentistry/Single/");
+          images(imagesListRef);
+        }
+        if(selected2 === 'مشتركة'){
+          const imagesListRef = ref(storage, "LibraryOfTheCollegeOfDentistry/Other/");
+          images(imagesListRef);
+        }
+      }
+
+      if(selected === "مكتبة العيادات الخارجية مستشفى الملك خالد"){
+        if(selected2 === 'فردية'){
+          const imagesListRef = ref(storage, "KingKhalidUniversityHospitalLibrary/Single/");
+          images(imagesListRef);
+        }
+        if(selected2 === 'مشتركة'){
+          const imagesListRef = ref(storage, "KingKhalidUniversityHospitalLibrary/Other/");
+          images(imagesListRef);
+        }
+      }
+
+      if(selected === "مكتبة كلية التربية"){
+        if(selected2 === 'فردية'){
+          const imagesListRef = ref(storage, "FACULTYOFEDUCATIONLibrary/Single/");
+          images(imagesListRef);
+        }
+        if(selected2 === 'مشتركة'){
+          const imagesListRef = ref(storage, "FACULTYOFEDUCATIONLibrary/Other/");
+          images(imagesListRef);
+        }
+      }
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+    // const [allDocs, setAllDocs] = useState([]);
+
+  //   useEffect( ()=>{
+  //     let _allDocs = [];
+  //     getDocs(colRefHall)
+  //      .then((snapshot) => {
+  //          snapshot.docs.forEach((doc) => {
+  //          const document = doc.data();
+  //            _allDocs.push({ ...doc.data(), id: doc.id})       
+           
+  //        if (document.hallType === selected2 && document.hallName === selected3){
+  //          setAllDocs(_allDocs);
+  //        }
+  //       })
+  // })
+  // .catch(err => {
+  //  console.log(err.message)
+  // })
+  //   })
     const handleSubmit = async () => {//function to prevent refresh page when click save
       await addDoc(colRefReserve, {
         HallName: selected3,
@@ -67,11 +192,19 @@ function Reservation() {
               <h2>اسم القاعة</h2>
             </div>
             <div className="hall">
-  
               <Time selected={selectedTime} setSelected={setSelectedTime} selectedLib={selected} 
                   selectedType={selected2} selectedHall={selected3} selectedDate={selectedDate}/>
               <h2>الوقت</h2>
             </div>
+            
+            <div className="hall">
+            {imageUrls.map((url) => {
+        // eslint-disable-next-line jsx-a11y/alt-text
+        return <img src={url} />;
+      })}
+      
+        </div>
+
             <div className="read-more-btn-box">
               { selected !== "اختاري مكتبة" && 
               selected2 !== "اختاري نوع القاعة" && 
